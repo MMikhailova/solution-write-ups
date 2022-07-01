f563571b7b319069cdc9ca155b20f5487799532b
Maria <naplesaries2014@gmail.com>
2022-07-01 18:36:02 +0200

    added solutions

--- repeat-string/sandbox.test.js
+++ repeat-string/sandbox.test.js
@@ -7,8 +7,8 @@
  * @returns {string} Repeated text.
  */
 
-const repeatString = (text = '', repetitions = 1) => {
-  const finalLength = text * repetitions;
+const repeatString1 = (text = '', repetitions = 1) => {
+  const finalLength = text.length * repetitions;
   let repeatedText = '';
   while (true) {
     if (repeatedText.length === finalLength) {
@@ -19,11 +19,43 @@ const repeatString = (text = '', repetitions = 1) => {
   return repeatedText;
 };
 
-for (const solution of [repeatString]) {
+const repeatString2 = (text = '', repetitions = 1) => {
+  const finalLength = text.length * repetitions;
+  let repeatedText = '';
+  while (repeatedText.length < finalLength) {
+    repeatedText += text;
+  }
+  return repeatedText;
+};
+
+const repeatString3 = (text = '', repetitions = 1) => {
+  let repeatedText = '';
+  for (let count = repetitions; count > 0; count--) {
+    repeatedText += text;
+  }
+  return repeatedText;
+};
+
+const repeatString4 = (text = '', repetitions = 1) => {
+  let repeatedText = '';
+  let count = 0;
+  while (count < repetitions) {
+    repeatedText += text;
+    count++;
+  }
+  return repeatedText;
+};
+
+for (const solution of [
+  repeatString1,
+  repeatString2,
+  repeatString3,
+  repeatString4,
+]) {
   describe(`${solution.name}: should repeat a text given number of times`, () => {
     describe('should return the string of repeated text', () => {
       it('"hello" to repeat 3 times should return "hellohellohello"', () => {
-        expect(solution('hello', 5).toEqual('hellohellohello'));
+        expect(solution('hello', 3)).toEqual('hellohellohello');
       });
     });
   });
